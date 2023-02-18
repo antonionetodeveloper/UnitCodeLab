08612a5686afaee32142d7d45a2f04e697a85445
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-18 16:46:26 -0300

    Create RelevantPosts.ts

---
+++ pages/api/posts/RelevantPosts.ts
@@ -0,0 +1,27 @@
+import { CORS } from "../../../middleware/Cors"
+import { ConnectDB } from "../../../middleware/ConnectDB"
+import { NextApiRequest, NextApiResponse } from "next"
+import { PostModule } from "../../../models/posts"
+
+const GetRelevantPosts = async (
+	request: NextApiRequest,
+	response: NextApiResponse,
+) => {
+	if (request.method == "GET") {
+		// Get the 10 most popular discussions
+		const Posts = await PostModule.find().sort({ comentsCont: -1 }).limit(10)
+
+		if (!Posts) {
+			return response
+				.status(400)
+				.json({ error: "Não foi possivel encontrar os posts." })
+		}
+
+		return response
+			.status(200)
+			.json({ message: "Posts encontrados com sucesso!", Posts })
+	}
+	return response.status(405).json({ error: "Método inválido." })
+}
+
+export default CORS(ConnectDB(GetRelevantPosts))
