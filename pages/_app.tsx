879fc2a34188a3db82d2a72709ef4c5337be8b26
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-18 19:58:41 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,30 @@
 import type { AppProps } from "next/app"
+import { ThemeProvider, DefaultTheme } from "styled-components"
+import GlobalStyle from "../styles/GlobalStyle"
+import { Context } from "./context/Context"
+import LoaderContainer from "./_loadContainer"
+
+const theme: DefaultTheme = {
+	colors: {
+		primary: "#fff",
+		secondary: "#fff",
+	},
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
-			<link
-				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-				rel="stylesheet"
-			/>
-			<Component {...pageProps} />
-		</>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Context>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Sono:wght@300;400;500;600;700;800&display=swap"
+					rel="stylesheet"
+				/>
+
+				<LoaderContainer Component={Component} pageProps={pageProps} />
+			</Context>
+		</ThemeProvider>
 	)
 }
