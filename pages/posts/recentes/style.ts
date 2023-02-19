e5d2b10ed2ee24f9a9b854414ac4af8e0bf12989
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-19 13:53:17 -0300

    semantic update

--- pages/posts/recentes/style.ts
+++ pages/posts/recentes/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const Container = styled.div`
+export const Container = styled.main`
 	min-height: 90vh;
 	background-color: #06021d;
 	color: white;
