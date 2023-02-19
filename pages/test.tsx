1c9ec7a5f6a987afeb49290c0ba04437a0fd3d72
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-18 23:40:25 -0300

    Revert "Update test.tsx"
    This reverts commit 43d126f34458510e631d12b0540053a95367979a.

--- pages/test.tsx
+++ pages/test.tsx
@@ -8,26 +8,19 @@ export default function Teste() {
 	}
 
 	const handle = async () => {
-		await axios
-			.post(
-				"https://unitcodelab.vercel.app/api/posts2/CreatePost",
-				{
-					title: obj.title,
-					content: obj.content,
-					commentsCount: obj.commentsCount,
+		await axios.post(
+			"https://unitcodelab.vercel.app/api/posts2/CreatePost",
+			{
+				title: obj.title,
+				content: obj.content,
+				commentsCount: obj.commentsCount,
+			},
+			{
+				headers: {
+					"Content-Type": "application/json",
 				},
-				{
-					headers: {
-						"Content-Type": "application/json",
-					},
-				},
-			)
-			.then((response) => {
-				console.log(response)
-			})
-			.catch((error) => {
-				console.log("error: ", error)
-			})
+			},
+		)
 	}
 
 	return (
