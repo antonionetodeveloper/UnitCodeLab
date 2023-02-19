bbc30e30934a13e63c47a49e897f6bf967e5f4f6
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-19 19:18:03 -0300

    split style of thecomponent

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react"
 import axios from "axios"
-import styled from "styled-components"
 import { API_URL } from "../../pages/_document"
 import { Item } from "./Item"
+import Container from "./style"
 
 /* eslint-disable react/no-unknown-property */
 const Search = () => {
@@ -103,136 +103,5 @@ const Search = () => {
 		</Container>
 	)
 }
-
-const Container = styled.div`
-	/* From uiverse.io by @satyamchaudharydev */
-	/* removing default style of button */
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-
-	.form button {
-		border: none;
-		background: none;
-		color: #000000;
-	}
-
-	.search {
-		:hover {
-			cursor: text;
-		}
-	}
-
-	/* styling of whole input container */
-	.form {
-		--timing: 0.3s;
-		--width-of-input: 200px;
-		--height-of-input: 40px;
-		--border-height: 2px;
-		--input-bg: #fff;
-		--border-color: #000000;
-		--border-radius: 30px;
-		--after-border-radius: 1px;
-		position: relative;
-		width: var(--width-of-input);
-		height: var(--height-of-input);
-		display: flex;
-		align-items: center;
-		padding-inline: 0.8em;
-		border-radius: var(--border-radius);
-		transition: border-radius 0.5s ease;
-		background: var(--input-bg, #fff);
-
-		:hover {
-			cursor: text;
-		}
-	}
-	/* styling of Input */
-	.input {
-		font-size: 0.9rem;
-		background-color: transparent;
-		width: 100%;
-		height: 100%;
-		padding-inline: 0.5em;
-		padding-block: 0.7em;
-		border: none;
-	}
-	/* styling of animated border */
-	.form:before {
-		content: "";
-		position: absolute;
-		background: var(--border-color);
-		transform: scaleX(0);
-		transform-origin: center;
-		width: 100%;
-		height: var(--border-height);
-		left: 0;
-		bottom: 0;
-		border-radius: 1px;
-		transition: transform var(--timing) ease;
-	}
-	/* Hover on Input */
-	.form:focus-within {
-		border-radius: var(--after-border-radius);
-	}
-
-	input:focus {
-		outline: none;
-	}
-	/* here is code of animated border */
-	.form:focus-within:before {
-		transform: scale(1);
-	}
-	/* styling of close button */
-	/* == you can click the close button to remove text == */
-	.reset {
-		border: none;
-		background: none;
-		opacity: 0;
-		visibility: hidden;
-		:hover {
-			cursor: pointer;
-		}
-	}
-	/* close button shown when typing */
-	input:not(:placeholder-shown) ~ .reset {
-		opacity: 1;
-		visibility: visible;
-	}
-	/* sizing svg icons */
-	.form svg {
-		width: 17px;
-		margin-top: 3px;
-	}
-
-	.options {
-		position: absolute;
-		max-height: 45vh;
-		min-height: 15vh;
-		width: 25vw;
-		top: 70px;
-
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-
-		ul {
-			padding-inline-start: 0px;
-			background-color: white;
-		}
-	}
-
-	.notFound {
-		font-family: "Sono";
-		padding: 2vw;
-		text-align: center;
-		transition: 0.3s;
-
-		:hover {
-			cursor: not-allowed;
-			background-color: #ccc;
-		}
-	}
-`
 
 export default Search
