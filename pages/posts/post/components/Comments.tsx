b9e2ab4bb8c57e7ce620f9e22862262ead93ca97
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-20 14:57:36 -0300

    create comment component

---
+++ pages/posts/post/components/Comments.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import styled from "styled-components"
+
+const Comments = ({ Comments }) => {
+	return (
+		<Container>
+			{Comments.map((comment) => (
+				<ul>
+					<li>
+						<h6>{comment.Comment}</h6>
+					</li>
+					<ul>
+						{comment.nextLevel.length > 0 ? (
+							comment.nextLevel.map((comment) => <li>{comment.Comment}</li>)
+						) : (
+							<></>
+						)}
+					</ul>
+				</ul>
+			))}
+		</Container>
+	)
+}
+
+const Container = styled.section``
+
+export default Comments
