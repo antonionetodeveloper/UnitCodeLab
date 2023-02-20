fe9f32edfbcd6f50410abc5012985311f55b7c23
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-19 23:11:30 -0300

    try to fix

--- pages/api/posts/SinglePost.ts
+++ pages/api/posts/SinglePost.ts
@@ -6,12 +6,15 @@ import { PostModule } from "../../../models/posts"
 const GetPost = async (request: NextApiRequest, response: NextApiResponse) => {
 	if (request.method == "POST") {
 		const { PostID } = await request.body
-		const Post = await PostModule.findById(PostID)
-		Post.__v = null
-
-		return response
-			.status(200)
-			.json({ message: "Post Encontrado com sucesso!", Post })
+		try {
+			const Post = await PostModule.findById(PostID)
+			Post.__v = null
+			return response
+				.status(200)
+				.json({ message: "Post Encontrado com sucesso!", Post })
+		} catch (error) {
+			return response.status(400).json({ error: "Erro. -> " + error })
+		}
 	}
 	return response.status(405).json({ error: "Método inválido." })
 }
