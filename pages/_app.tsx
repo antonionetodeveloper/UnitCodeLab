396d697f366400382b39af6f808ba925b0f88020
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-24 18:36:11 -0300

    Loading settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from "next/app"
+import { Router } from "next/router"
+import { useEffect, useState } from "react"
 import { ThemeProvider, DefaultTheme } from "styled-components"
+import Loader from "../components/Loader"
 import GlobalStyle from "../styles/GlobalStyle"
 import { Context } from "./context/Context"
-import LoaderContainer from "./_loadContainer"
+import DisplayContainer from "./_loadContainer"
 
 const theme: DefaultTheme = {
 	colors: {
@@ -12,11 +15,35 @@ const theme: DefaultTheme = {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		const start = () => {
+			console.log("start")
+			setLoading(true)
+		}
+		const end = () => {
+			console.log("findished")
+			setLoading(false)
+		}
+		Router.events.on("routeChangeStart", start)
+		Router.events.on("routeChangeComplete", end)
+		Router.events.on("routeChangeError", end)
+		return () => {
+			Router.events.off("routeChangeStart", start)
+			Router.events.off("routeChangeComplete", end)
+			Router.events.off("routeChangeError", end)
+		}
+	}, [])
+
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<Context>
-				<LoaderContainer Component={Component} pageProps={pageProps} />
+				{loading ? (
+					<Loader />
+				) : (
+					<DisplayContainer Component={Component} pageProps={pageProps} />
+				)}
 			</Context>
 		</ThemeProvider>
 	)
--- pages/_loadContainer.tsx
+++ pages/_loadContainer.tsx
@@ -3,7 +3,7 @@ import LoadingContext from "./context/Context"
 import Loader from "../components/Loader"
 
 // eslint-disable-next-line react/prop-types
-export default function LoaderContainer({ Component, pageProps }) {
+export default function DisplayContainer({ Component, pageProps }) {
 	const CONTEXT = useContext(LoadingContext)
 	useEffect(() => {
 		setTimeout(() => CONTEXT.setLoading(false), 2000)
