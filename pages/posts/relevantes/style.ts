cbdbaaed289fbf820072f33a3c35e2c3307f1a25
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-24 19:09:37 -0300

    Update style.ts

--- pages/posts/relevantes/style.ts
+++ pages/posts/relevantes/style.ts
@@ -10,5 +10,10 @@ export const Container = styled.main`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+
+	h2 {
+		font-size: 2.5rem;
+		font-weight: 900;
+	}
 `
 export default Container
