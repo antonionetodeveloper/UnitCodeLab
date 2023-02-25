23e8cdde0506df4206fb4be75e7ee85a7a9b820f
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-24 23:09:31 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,57 +1,70 @@
 import Link from "next/link"
 import { useContext } from "react"
 import styled from "styled-components"
-import LoadingContext from "../pages/context/Context"
+import Context from "../pages/context/Context"
+import RegularButton from "./Buttons/RegularButton"
 import Search from "./search/Search"
 
 const Header = () => {
-	const { selectedHeaderItem } = useContext(LoadingContext)
+	const { selectedHeaderItem, auth } = useContext(Context)
 
 	return (
 		<Container SelectedItem={selectedHeaderItem}>
 			<nav>
 				<div className="leftSideDiv">
 					<div className="hooks">
-						<span>
+						<div>
 							<Link href="/">
-								<a href="/" className="inicio">
-									Início
-								</a>
+								<p className="inicio">Início</p>
 							</Link>
-						</span>
+						</div>
 
-						<span>
+						<div>
 							<Link href="/posts/recentes/">
-								<a href="/posts/recentes/" className="recentes">
-									Recentes
-								</a>
+								<p className="recentes">Recentes</p>
 							</Link>
-						</span>
+						</div>
 
-						<span>
+						<div>
 							<Link href="/posts/relevantes/">
-								<a href="/posts/relevantes/" className="relevantes">
-									Relevantes
-								</a>
+								<p className="relevantes">Relevantes</p>
 							</Link>
-						</span>
+						</div>
 
-						<span>
-							<Link href="/posts/relevantes/">
-								<a
-									href="https://github.com/antonionetodeveloper/UnitCodeLab"
-									className="api"
-									target="_blank"
-									rel="noreferrer"
-								>
-									API
-								</a>
-							</Link>
-						</span>
+						<div>
+							<a
+								href="https://github.com/antonionetodeveloper/UnitCodeLab"
+								className="api"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<p>API</p>
+							</a>
+						</div>
 					</div>
 					<Search />
 				</div>
-				<div></div>
+				<div>
+					{auth ? (
+						<>
+							<Link href="/">
+								<RegularButton>Criar uma discussão</RegularButton>
+							</Link>
+							<Link href="/">
+								<img src="/assets/user.png" alt="usuario" />
+							</Link>
+						</>
+					) : (
+						<>
+							<Link href="/usuario/">
+								<RegularButton>Criar uma discussão</RegularButton>
+							</Link>
+							<Link href="/usuario/">
+								<img src="/assets/user.png" alt="usuario" />
+							</Link>
+						</>
+					)}
+				</div>
 			</nav>
 		</Container>
 	)
@@ -69,7 +82,34 @@ const Container: any = styled.header`
 	z-index: 3;
 
 	nav {
+		width: 95%;
 		margin-left: 2vw;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		div {
+			display: flex;
+			align-items: center;
+			gap: 2vw;
+
+			button {
+				width: 100%;
+				height: 6vh;
+			}
+
+			img {
+				width: 10%;
+				padding: 0.5vw;
+				border-radius: 50%;
+				transition: 0.3s;
+
+				:hover {
+					background-color: #ccc;
+					cursor: pointer;
+				}
+			}
+		}
 
 		.leftSideDiv {
 			display: flex;
@@ -82,11 +122,11 @@ const Container: any = styled.header`
 				gap: 2vw;
 			}
 
-			span {
+			div {
 				display: flex;
 				align-items: center;
 
-				a {
+				p {
 					letter-spacing: 1px;
 					font-size: 1.2vw;
 					font-weight: 700;
@@ -94,26 +134,27 @@ const Container: any = styled.header`
 
 					:hover {
 						text-decoration: underline;
+						cursor: pointer;
 					}
+				}
+				a.api {
+					text-decoration: none;
 				}
 			}
 		}
 	}
 
-	a.inicio {
+	p.inicio {
 		text-decoration: ${(props: any) =>
 			props.SelectedItem == "inicio" ? "underline" : "none"};
 	}
-	a.recentes {
+	p.recentes {
 		text-decoration: ${(props: any) =>
 			props.SelectedItem == "recentes" ? "underline" : "none"};
 	}
-	a.relevantes {
+	p.relevantes {
 		text-decoration: ${(props: any) =>
 			props.SelectedItem == "relevantes" ? "underline" : "none"};
-	}
-	a.api {
-		text-decoration: none;
 	}
 `
 
