5f5e07213d0f3342adba9d828d78045521087e10
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-03-19 22:13:28 -0300

    try to fix

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -3,7 +3,13 @@ import { ConnectDB } from "../../middleware/ConnectDB"
 import { NextApiRequest, NextApiResponse } from "next"
 
 const endPoint = async (request: NextApiRequest, response: NextApiResponse) => {
-	return response.status(200).json({ success: true, message: "Working!" })
+	try {
+		return response.status(200).json({ success: true, message: "Working!" })
+	} catch (error) {
+		return response
+			.status(400)
+			.json({ success: false, message: "Not Working!", error: error })
+	}
 }
 
 export default CORS(ConnectDB(endPoint))
