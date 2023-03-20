ab83410dc4fc30d93a6c24678649173e648bddd2
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-03-19 21:44:30 -0300

    Revert "revert all"
    This reverts commit 77386184a03130e70a0d89d4ffc568958ac38807.

--- middleware/Cors.ts
+++ middleware/Cors.ts
@@ -1,17 +1,18 @@
+import { cors } from "cors"
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next"
 import type { RegularAnswear } from "../types/RegularAnswear"
-import NextCors from "nextjs-cors"
+
+const Cors = cors({
+	origin: "https://www.unitedcodelab.tech",
+	methods: ["GET", "POST", "PUT"],
+	allowedHeaders: ["Content-Type"],
+})
 
 export const CORS =
 	(handler: NextApiHandler) =>
 	async (req: NextApiRequest, res: NextApiResponse<RegularAnswear>) => {
 		try {
-			await NextCors(req, res, {
-				origin: "*",
-				methods: ["GET", "POST", "PUT"],
-				optionsSuccessStatus: 200, // navegadores antigos dao problema quando se retorna 204
-			})
-
+			await Cors(req, res)
 			return handler(req, res)
 		} catch (e) {
 			console.log("Erro ao tratar a politica de CORS: ", e)
